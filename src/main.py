ed0787fb252b2bd6ac6ff6c60bde66f35be01f65
dharana77 <dharana7723@gmail.com>
2021-08-14 20:01:58 +0900

    path order test

--- src/main.py
+++ src/main.py
@@ -22,3 +22,11 @@ def read_item(item_id: int, q: Optional[str] = None):
 def update_item(item_id:int, item:Item):
     return {"item_name": item.name, "item_id":item_id}
 
+@app.get("users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id")
+async def read_user(user_id:str):
+    return {"user_id": user_id}
+
