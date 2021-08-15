578d0112e6eff7b4f33c68975476eaa5783f107e
dharana77 <dharana7723@gmail.com>
2021-08-15 23:39:53 +0900

    쿼리 파라미터 테스트

--- src/main.py
+++ src/main.py
@@ -15,12 +15,20 @@ class ModelName(str, Enum):
     resnet = "resnet"
     lenet = "lenet"
 
+fake_items_db = [{"item_name":"Foo"}, {"items":"Bar"}, {"item_name": "Baz"}]
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/items/")
+async def read_item(skip:int =0 , limit:int =10):
+    return fake_items_db[skip: skip+limit]
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id , "q": q}
     return {"item_id": item_id, "q":q}
 
 @app.put("/items/{items_id}")
