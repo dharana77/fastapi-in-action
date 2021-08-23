348adf592112b50ff204d42e63c967cb9c8ff71b
dharana77 <dharana7723@gmail.com>
2021-08-23 23:50:27 +0900

    extra data types

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ from typing import List, Optional, Set
 from pydantic import BaseModel, Field
 from enum import Enum
 from pydantic import BaseModel, HttpUrl, Field
+from datetime import datetime, time, timedelta
+from uuid import UUID
 
 app = FastAPI()
 
@@ -15,7 +17,7 @@ class Item(BaseModel):
     description: Optional[str] = Field(None, example="A very nice Item")
     price: float = Field(..., example=35.4)
     tax: Optional[float] = Field(None, example=3.2)
-    
+
     class Config:
         schema_extra = {
             "example":{
@@ -45,15 +47,24 @@ async def create_item(q: str = Query("fixedquery", min_length=3)):
     return results
 
 @app.put("items/{item_id}")
-async def update_item(
-    item_id: int , item:Item, user:User, importance: int = Body(...)
+async def read_items(
+    item_id: UUID,
+    start_datetime: Optional[datetime] = Body(None),
+    end_datetime : Optional[datetime] = Body(None),
+    repeat_at : Optional[time] = Body(None),
+    process_after: Optional[timedelta] = Body(None),
 ):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_After": process_after,
+        "start_process": start_process,
+        "duartion": duartion,
+    }
 
 @app.post("/offers/")
 async def create_offer(offer: Offer):
